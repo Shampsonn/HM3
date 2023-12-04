59bf0d4dd1fb597a61b7397479c0b72085e350e3
Alexander Bortsov <A.bortsovv@yandex.ru>
2023-12-04 21:22:13 +0300

    Git Init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 13676;
+        int bonus;
+        bonus = ticket / 20;
+        int miles = bonus;
+        System.out.println(miles);
+        }
+    }
